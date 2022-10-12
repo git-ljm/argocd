26a97fef091c266a7a211160dc66e00f51665098
git-ljm <88469127+git-ljm@users.noreply.github.com>
2022-10-12 10:34:41 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-mount
-echo "hello world"
-df -h
+#!/usr/bin/env bash
+export tmp_data_directory="/tmp"
+export data_directory="/data"
+
+set -ex
+
+cd /tmp/;
+aria2c -x8 -s8  -o /tmp/latest https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.17.2.tar.xz
+tar -xvf /tmp/latest -C /data/ --strip 1
 
